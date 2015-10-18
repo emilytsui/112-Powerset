cbe4b2deb0473448799cc3440d7618c464ec0910
GRMmouse <ail1@andrew.cmu.edu>
2015-10-18 06:49:06 -0400

    Last commit this morning before 9, probably....
    Implemented the click and italicize feature. Contain a minor bug that
    should not be hard to fix.
    Implemented prevStep during recursive call phase, did not implement
    prevStep for merge part yet.
    Removed old code for old question

--- exercises/powerset/main.js
+++ exercises/powerset/main.js
@@ -48,7 +48,7 @@ var main = function(ex) {
                                     "Next").on("click", nextStep)
     var prevButton = ex.createButton(canvasWidth*(9/11)-60, canvasHeight*(9/10),
                                      "Prev").on("click", prevStep)
-    prevButton.disable(); //Not implemented correctly for right now
+    //prevButton.disable(); //Not implemented correctly for right now
     var skipButton = ex.createButton(canvasWidth*(11/12), canvasHeight*(9/10),
                                      "Skip").on("click", skipStep)
     var quizButton = ex.createButton(canvasWidth*(8/9)-10, 10,
@@ -122,26 +122,28 @@ var main = function(ex) {
     }
 
     //generate recursiveCall data
-    powersetMain([1,2,3])
-    console.log(state.recursiveCalls)
+    powersetMain(generateList());
+    console.log(state.recursiveCalls);
 
     //perform appropriate action after prevButton is clicked
     function prevStep() { //don't think this is implemented correctly
-        if (state.recursiveDepth < 0){
+        if (state.recursiveDepth <= 0){
             //Do nothing at depth 0
             return;
         }else{
             if (!state.isReturning){
-                var thisCall = state.recursiveCalls[state.recursiveDepth];
-                thisCall.h1.remove();
-                thisCall.h2.remove();
-                thisCall.h3.remove();
-                drawCall();
                 state.recursiveDepth--;
+                var thisCall = state.recursiveCalls[state.recursiveDepth];
+                if (state.recursiveDepth <= state.listLength){
+                    thisCall.h1.remove();
+                    thisCall.h2.remove();
+                    thisCall.h3.remove();
+                }else{
+                    thisCall.h1.remove();
+                }
             }else{
                 //if next step is to merge
                 if (state.isMerging) {
-                    drawReturn();//This may need to be changed!!
                     state.isMerging = false;
                 } else {
                 //Normal return
@@ -367,7 +369,7 @@ var main = function(ex) {
                 ex.data.state.questionNum = 1;
                 nextQButton.disable();
                 submitQButton.disable();
-                drawQ1();
+                drawQuestion();
             }
             state.recursiveDepth++;
         } else {
@@ -428,56 +430,64 @@ var main = function(ex) {
         submitQButton.disable();
     }
 
-    var option1, option2, option3, option4;
-    function drawQ1() {
-        // There should be a make questio functio instead of making one
+
+    function drawQuestion() {
+        // There should be a make questio function instead of making one
         // for each question
+        for (var i =0; i<questionObjects.length; i++){
+            questionObjects[i].remove()
+        }
+        var styles = ["normal", "normal", "normal", "normal"];
+        switch(ex.data.question1.selected){
+            case "a":
+                styles[0] = "italic";
+                break;
+            case "b":
+                styles[1] = "italic";
+                break;
+            case "c":
+                styles[2] = "italic";
+                break;
+            case "d":
+                styles[3] = "italic";
+                break;
+            default:
+                break;
+        }
         var xOrigin = sideMargin + blockWidth * (state.recursiveDepth+1);
         var yOrigin = topMargin + (state.recursiveDepth+1) * 1.5 * lineHeight;
         var question = ex.createParagraph(xOrigin, yOrigin,
                             ex.data.question1.question, {size: "large"});
         var option1 = ex.createParagraph(xOrigin, yOrigin+lineHeight,
-                    ex.data.question1.options[0], {size: "large"}).on("click",
+                    ex.data.question1.options[0], {size: "large", fontStyle: styles[0]}).on("click",
                     function() {
-                        option1.style()["fontStyle"] = "italic";
-                        option2.style()["fontStyle"] = "normal";
-                        option3.style()["fontStyle"] = "normal";
-                        option4.style()["fontStyle"] = "normal";
                         ex.data.question1.selected = "a";
                         submitQButton.enable();
+                        drawQuestion();
                         console.log(ex.data.question1.selected);
                     });
         var option2 = ex.createParagraph(xOrigin, yOrigin+lineHeight*2,
-                    ex.data.question1.options[1], {size: "large"}).on("click",
+                    ex.data.question1.options[1], {size: "large", fontStyle: styles[1]}).on("click",
                     function() {
-                        option1.style()["fontStyle"] = "normal";
-                        option2.style()["fontStyle"] = "italic";
-                        option3.style()["fontStyle"] = "normal";
-                        option4.style()["fontStyle"] = "normal";
                         ex.data.question1.selected = "b";
                         submitQButton.enable();
+                        drawQuestion();
                         console.log(ex.data.question1.selected);
                     });
         var option3 = ex.createParagraph(xOrigin, yOrigin+lineHeight*3,
-                    ex.data.question1.options[2], {size: "large"}).on("click",
+                    ex.data.question1.options[2], {size: "large", fontStyle: styles[2]}).on("click",
                     function() {
-                        option1.style()["fontStyle"] = "normal";
-                        option2.style()["fontStyle"] = "normal";
-                        option3.style()["fontStyle"] = "italic";
-                        option4.style()["fontStyle"] = "normal";
                         ex.data.question1.selected = "c";
                         submitQButton.enable();
+                        drawQuestion();
                         console.log(ex.data.question1.selected);
                     });
         var option4 = ex.createParagraph(xOrigin, yOrigin+lineHeight*4,
-                    ex.data.question1.options[3], {size: "large"}).on("click",
+                    ex.data.question1.options[3], {size: "large", fontStyle: styles[3]}).on("click",
                     function() {
-                        option1.style()["fontStyle"] = "normal";
-                        option2.style()["fontStyle"] = "normal";
-                        option3.style()["fontStyle"] = "italic";
-                        option4.style()["fontStyle"] = "normal";
                         ex.data.question1.selected = "d";
                         submitQButton.enable();
+                        drawQuestion();
                         console.log(ex.data.question1.selected);
                     });
         questionObjects.push(question);
@@ -485,121 +495,8 @@ var main = function(ex) {
         questionObjects.push(option2);
         questionObjects.push(option3);
         questionObjects.push(option4);
+           
     }
-
-    //////// The following code was used for the old version of the quiz
-    ////// Will be removed once the new version works
-    // var question;
-    // var code;
-    // var input;
-    // var selections;
-    // var submitQButton;
-
-    // function eraseQuestion() {
-    //     question.remove();
-    //     code.remove();
-    //     input.remove();
-    //     for (var i = 0; i < selections.length; i++) {
-    //         selections[i].remove();
-    //     }
-    //     submitQButton.remove();
-    // }
-
-    // function selectAnswer(selections, i, qNum) {
-    //     selection = selections[i];
-    //     selection.style()["fontStyle"] = "italic";
-    //     if (i == 0 && qNum == 1) ex.data.question1.selected = "a";
-    //     else if (i == 1 && qNum == 1) ex.data.question1.selected = "b";
-    //     else if (i == 2 && qNum == 1) ex.data.question1.selected = "c";
-    //     else if (i == 3 && qNum == 1) ex.data.question1.selected = "d";
-    //     else if (i == 0) ex.data.question2.selected = "a";
-    //     else if (i == 1) ex.data.question2.selected = "b";
-    //     else if (i == 2) ex.data.question2.selected = "c";
-    //     else if (i == 3) ex.data.question2.selected = "d";
-    //     for (var j = 0; j < selections.length; j++) {
-    //         if (i == j) continue;
-    //         selections[j].style()["fontStyle"] = "normal";
-    //     }
-    // }
-
-    // //displays question 1 and all its components
-    // function drawQ1() {
-    //     nextQButton = ex.createButton(canvasWidth*(10/11),
-    //             canvasHeight*(9/10), "Next").on("click", nextQuestion);
-    //     nextQButton.disable();
-
-    //     ex.graphics.ctx.moveTo(canvasWidth/2, 0);
-    //     ex.graphics.ctx.lineTo(canvasWidth/2, canvasHeight);
-    //     ex.graphics.ctx.stroke();
-
-    //     question = ex.createParagraph(10,10,ex.data.question1.question,
-    //                             {size: "large"});
-    //     code = ex.createCode(10, canvasHeight/3,
-    //                 ex.data.question1.code.display, ex.data.question1.code);
-    //     input = ex.createInputText(canvasWidth/2+20,20,"Answer");
-    //     selections = drawSelections(ex.data.question1.options, canvasWidth/2+20, 100, 100, "large");
-    //     // for (var i = 0; i < selections.length; i++) {
-    //     //     selections[i].on("click", function() {
-    //     //         selectAnswer(selections[i], i, 1);
-    //     //     });
-    //     // }
-    //     submitQButton = ex.createButton(canvasWidth*(10/11)-15, 20,
-    //         "Submit").on("click", function() {
-    //                 submitQButton.disable();
-    //                 input.disable();
-    //                 nextQButton.enable();
-    //                 ex.data.question1.selected = input.text().trim();
-    //                 if (ex.data.question1.answer == ex.data.question1.selected) {
-    //                     ex.showFeedback("Correct!");
-    //                 }
-    //                 else ex.showFeedback("Incorrect. Step through the visualization again to see why.");
-    //             });
-    // }
-
-    // function drawQ2() {
-    //     nextQButton = ex.createButton(canvasWidth*(10/11),
-    //             canvasHeight*(9/10), "Next").on("click", nextQuestion);
-    //     nextQButton.disable();
-
-    //     ex.graphics.ctx.moveTo(canvasWidth/2, 0);
-    //     ex.graphics.ctx.lineTo(canvasWidth/2, canvasHeight);
-    //     ex.graphics.ctx.stroke();
-
-    //     question = ex.createParagraph(10,10,ex.data.question2.question,
-    //                             {size: "large"});
-    //     codeWell = ex.createCode(10, canvasHeight/3,
-    //                 ex.data.question2.code.display, ex.data.question2.code);
-    //     codeWell.width(canvasWidth/2-40);
-    //     input = ex.createInputText(canvasWidth/2+20,20,"Answer");
-    //     selections = drawSelections(ex.data.question2.options, canvasWidth/2+20, 100, 100, "large");
-    //     // for (var i = 0; i < selections.length; i++) {
-    //     //     selections[i].on("click", function() {
-    //     //         selectAnswer(selections[i], i, 1);
-    //     //     });
-    //     // }
-    //     submitQButton = ex.createButton(canvasWidth*(10/11)-15, 20,
-    //         "Submit").on("click", function() {
-    //                 submitQButton.disable();
-    //                 input.disable();
-    //                 nextQButton.enable();
-    //                 ex.data.question2.selected = input.text().trim();
-    //                 if (ex.data.question2.answer == ex.data.question2.selected) {
-    //                     ex.showFeedback("Correct!");
-    //                 }
-    //                 else ex.showFeedback("Incorrect. Step through the visualization again to see why.");
-    //             });
-    // }
-
-    // // Creates paragraph objects of the question selections
-    // function drawSelections(selections, startX, startY, increment, size) {
-    //     var options = new Array();
-    //     for (var i = 0; i < selections.length; i ++) {
-    //         var a = ex.createParagraph(startX, startY + increment*i,
-    //                                    selections[i], {size: size});
-    //         options.push(a);
-    //     }
-    //     return options;
-    // }
 
 
 };
